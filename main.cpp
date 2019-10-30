b7dac7659d4a6f0815597d4b7a230e67297008b2
Bulakh Igor <igorbulakh1987@yandex.ru>
2019-10-30 12:48:45 +0300

    Changed travis file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
+#include <iostream>
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+//#include <spdlog/spdlog.h>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
-
+//    auto logger = spdlog::stdout_logger_mt("console");
+//    logger->info("version {} was started", version());
+    std::cout << "build " << std::endl;
+    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
